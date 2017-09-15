2e9da48a692b7a268652f11290ebf969eaa224c8
Woo Seok Yang <wooseok.yang@slu.edu>
2017-09-15 04:36:01 -0500

    finish

--- MyFifteenPuzzle.py
+++ MyFifteenPuzzle.py
@@ -13,7 +13,16 @@ class MyFifteenPuzzle(FifteenPuzzle):
 
 		'''
                 Using the heuristic to count the misplacement sums using the A* algorithm
-                Manhattan Distance algorithm 
+                Manhattan Distance algorithm
+
+                Manhattan Distance algorithm is effective as it is admissible, which means it never overestimates the optimal path in a graph or tree.
+
+                M. Distance calculates the distance between two points along the axes, that of each tile tothe tile in the original position.
+
+                This algorithm is effective because this takes into account how far the tile is from where it belongs and thus is more convincing than the number of misplaced tiles algorithm, which takes only the granted fact of the tiles being misplacd regarded
+
+                A* algorithm used in the MySearch perform function serves to expand the search tree by expanding th enode for which the past cost cost and the heuristic value is minimum.
+                The heuristic search and past cost make the lower bound on the cost of the path to the goal and the algorithm expands the search traversal only accordingly to the sum of the heuristic and the path cost every child node, meaning the path that A* search would take would always be less in the cost and heuristics together. 
                 '''
 		distance = 0
 		for row in range(4):
@@ -21,3 +30,6 @@ class MyFifteenPuzzle(FifteenPuzzle):
                                 if state[row][col] == 0: continue
                                 distance += abs(row - state[row][col]/4) + abs(col - state[row][col]%4)
                 return distance
+
+
+                
--- MySearch.py
+++ MySearch.py
@@ -25,9 +25,9 @@ class MySearch:
                                  
 		root = Node(problem.getInitial())
 		
-		queue = [root]	#front					# Setup the data structure for storing nodes to explore and put the root in it
+		queue = [root]					# Setup the data structure for storing nodes to explore and put the root in it
 
-		found = set() #expanded
+		found = set() 
 		found.add(problem.getInitial())
 		
 		if visualize:
@@ -40,30 +40,23 @@ class MySearch:
 		
 		while len(queue) > 0:
 
-                        #print 'entered the queue seq'
+                        '''Using A* algorithm using the manhattan distance of each block summing up'''
                         i = 0
                         for j in range(1, len(queue)):
-                                if heursitic(queue[i]) > heuristic(queue[j]):
+                                if problem.heuristic(queue[i].getState()) > problem.heuristic(queue[j].getState()):
                                         i = j
                         
                         path = queue[i]
-                        queue = queue[:i]+queue[i+1:]
-                        endnode = path
-
-
-                        
-			node = queue.pop(randint(0,len(queue)-1))	# Choose a random unexplored node
-			state = node.getState()
-			
+			node = queue.pop(i)	
+			state = path.getState()
+                        			
 			if problem.goalReached(state):
-				ellapsed = time() - startTime
-				print 'Solution found in', ellapsed, 'seconds'
+				elapsed = time() - startTime
+				print 'Solution found in', elapsed, 'seconds'
 				print 'Expanded', expanded, 'nodes.'
-				print 'Evaluation rate =', expanded/nodes, 'nodes/sec.'
+				print 'Evaluation rate =', expanded/elapsed, 'nodes/sec.'
 				return node.path()  # Return a list of actions to get to solved state
-
-                        if endnode in found: continue
-			
+                        
 			expanded += 1
                         
 			if expanded % 10000 == 0:
@@ -73,8 +66,10 @@ class MySearch:
 				print 'Selected node'
 				print problem.text(state)
 				print
-				
+
+			
 			found.add(state)
+			
 			
 			if visualize:
 				drawTree(problem, allNodes, problem.getInitial(), problem.getGoal(), state)
@@ -91,8 +86,9 @@ class MySearch:
 				if result not in found:
 					cost = problem.cost(state, action)
 					child = Node(result, node, action, cost)
+                                        
 
-					newpath = path + result
+					node.path().append(result)
 					queue.append(child)		# Add the child to the nodes to explore
 					found.add(result)
 					
